de8b09d9047cf6cf9838fdac2af8697d02d9f7a3
Gideon Ogongo <ogundareayodeji25@giddy-pc.local>
2016-07-02 13:09:26 +0200

    Puzzle Game Final

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,12 +1,13 @@
 import {Component} from '@angular/core';
-
+import {PuzzleComponent} from './puzzle/puzzle.component';
 @Component({
     selector: 'my-app',
     template: `
-        <h1>Angular 2 Boilerplate</h1>
-        <p>Hello World!!</p>
+        <my-puzzle>
+        </my-puzzle>
        
     `,
+    directives: [PuzzleComponent]
 })
 export class AppComponent {
 
